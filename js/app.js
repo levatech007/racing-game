543f1cf9eb5a68d86644cdcf13ade3d08d5d3666
Eva <evaliisagalen@gmail.com>
2017-12-06 15:37:55 -0800

    finished game functionality, still needs reset

--- js/app.js
+++ js/app.js
@@ -7,15 +7,16 @@ $(document).ready(function() {
   // };
 
 
-  let eggNames = ["egg1", "egg2", "egg3"]
-  let egg1Speed = Math.floor(Math.random() * 3500) + 2000 //creates random drop speeds
-  let egg2Speed = Math.floor(Math.random() * 3500) + 2000//creates random drop speeds
-  let egg3Speed = Math.floor(Math.random() * 3500) + 2000 //creates random drop speeds
+  //let eggNames = ["egg1", "egg2", "egg3"]
+  let egg1Speed = Math.floor(Math.random() * 3500) + 2000 // creates random drop speeds
+  let egg2Speed = Math.floor(Math.random() * 3500) + 2000 // creates random drop speeds
+  let egg3Speed = Math.floor(Math.random() * 3500) + 2000 // creates random drop speeds
 
   let egg1Delay = Math.floor(Math.random() * 1500) + 0
   let egg2Delay = Math.floor(Math.random() * 1500) + 0
   let egg3Delay = Math.floor(Math.random() * 1500) + 0
 
+  let didntReacheBottom = 0                     // counter for how many boxes reached bottom
   // $("button").on("click", function() { // clicking on
   //   for (let i = 0; i < eggNames.length ; i++) {
   //     $(`#${eggNames[i]}`).stop().delay(generateRandom(1500, 0)).animate({
@@ -24,7 +25,7 @@ $(document).ready(function() {
   //     console.log(`#${eggNames[i]}`);
   //   }
 
-  $("button").on("click", function() {
+  $("button").on("click", function() {          // clicking on START button creates function
 
       $("#egg1").stop().delay(egg1Delay).animate({
         "margin-top":"+=496px"
@@ -38,20 +39,14 @@ $(document).ready(function() {
         "margin-top":"+=496px"
       }, egg3Speed, "swing");
 
-    $(".cube").on("click", function(e) { //on click of the egg, it dissapears
-      $(this).css("opacity", "0");
-      
+      $(".cube").on("click", function(e) {      // on click of the egg, it dissapears (will change image)
+        didntReacheBottom += 1                  // if it is clicked on, it will not reach bottom and 1 is added to didntReacheBottom variable
+        $(this).css("opacity", "0");            // change something about the item if it is clicked on (change color, later image)
 
-
+          if (didntReacheBottom > 2) {          // check if all boxes did not reach bottom
+            alert("You win!");                  // if true, alert player that they won
+          };
+      });
+      //reset board
     });
-
   });
-
-  // on click set display to none or background color to none?
-  //if all reach bottom, alert LOSE message, else WIN. Reset game
-
-
-
-
-
-});
