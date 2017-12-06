aca78ad217f5ca01e38d08765df9bff35eac759f
Eva <evaliisagalen@gmail.com>
2017-12-06 14:26:38 -0800

    functioning egg drop, still needs cleaning

--- js/app.js
+++ js/app.js
@@ -2,28 +2,53 @@ console.log("js is linked");
 
 $(document).ready(function() {
 
+  // function generateRandom(x, y) {
+  //   Math.floor(Math.random() * x) + y;
+  // };
 
-  let dropSpeed1 = Math.floor(Math.random() * 3500) + 1500 //creates random drop speeds
-  let dropSpeed2 = Math.floor(Math.random() * 3500) + 1500 //creates random drop speeds
-  let dropSpeed3 = Math.floor(Math.random() * 3500) + 1500 //creates random drop speeds
 
-  $("button").on("click", function() { // clicking on button
-    $("#egg1").animate({
-      "margin-top":"+=396px"
-    }, dropSpeed1, "swing");
+  let eggNames = ["egg1", "egg2", "egg3"]
+  let egg1Speed = Math.floor(Math.random() * 3500) + 2000 //creates random drop speeds
+  let egg2Speed = Math.floor(Math.random() * 3500) + 2000//creates random drop speeds
+  let egg3Speed = Math.floor(Math.random() * 3500) + 2000 //creates random drop speeds
 
-    $("#egg2").animate({
-      "margin-top":"+=396px"
-    }, dropSpeed2, "swing");
+  let egg1Delay = Math.floor(Math.random() * 1500) + 0
+  let egg2Delay = Math.floor(Math.random() * 1500) + 0
+  let egg3Delay = Math.floor(Math.random() * 1500) + 0
 
-    $("#egg3").animate({
-      "margin-top":"+=396px"
-    }, dropSpeed3, "swing");
+  // $("button").on("click", function() { // clicking on
+  //   for (let i = 0; i < eggNames.length ; i++) {
+  //     $(`#${eggNames[i]}`).stop().delay(generateRandom(1500, 0)).animate({
+  //       "margin-top":"+=496px"
+  //     }, generateRandom(3500, 2000), "swing");
+  //     console.log(`#${eggNames[i]}`);
+  //   }
+
+  $("button").on("click", function() {
+
+      $("#egg1").stop().delay(egg1Delay).animate({
+        "margin-top":"+=496px"
+      }, egg1Speed, "swing");
+
+      $("#egg2").stop().delay(egg2Delay).animate({
+        "margin-top":"+=496px"
+      }, egg2Speed, "swing");
+
+      $("#egg3").stop().delay(egg3Delay).animate({
+        "margin-top":"+=496px"
+      }, egg3Speed, "swing");
+
+    $(".cube").on("click", function(e) { //on click of the egg, it dissapears
+      $(this).css("opacity", "0");
+      
+
+
+    });
 
   });
 
-  // on click set display to none or background color to none? 
-
+  // on click set display to none or background color to none?
+  //if all reach bottom, alert LOSE message, else WIN. Reset game
 
 
 
