2e85b96a860f0ad067e20a891ec2570aaad79c51
Eva <evaliisagalen@gmail.com>
2017-12-06 20:51:04 -0800

    removed unnecessary code and comments

--- js/app.js
+++ js/app.js
@@ -2,51 +2,37 @@ console.log("js is linked");
 
 $(document).ready(function() {
 
-  // function generateRandom(x, y) {
-  //   Math.floor(Math.random() * x) + y;
-  // };
+  let eggImage = `<img src="images/egg.svg" >`
+  let numOfElements = $(".cube").length
+  let didntReachBottom = 0    // counter for how many boxes reach bottom
 
+  function randomDuration(min, max) {                     // generates a random nuber for duration of jQuery .animate()
+    return Math.floor(Math.random() * (max-min) + min);
+  };
 
-  //let eggNames = ["egg1", "egg2", "egg3"]
-  let egg1Speed = Math.floor(Math.random() * 3500) + 2000 // creates random drop speeds
-  let egg2Speed = Math.floor(Math.random() * 3500) + 2000 // creates random drop speeds
-  let egg3Speed = Math.floor(Math.random() * 3500) + 2000 // creates random drop speeds
+  function animateEggs(idx) {
+    $(`#egg${idx}`).delay(randomDuration(0, 1500)).animate({
+      "margin-top":"+=496px"
+    },randomDuration(2000, 3500));
+  };
 
-  let egg1Delay = Math.floor(Math.random() * 1500) + 0
-  let egg2Delay = Math.floor(Math.random() * 1500) + 0
-  let egg3Delay = Math.floor(Math.random() * 1500) + 0
+  $(".cube").append(eggImage);                  
 
-  let didntReacheBottom = 0                     // counter for how many boxes reached bottom
-  // $("button").on("click", function() { // clicking on
-  //   for (let i = 0; i < eggNames.length ; i++) {
-  //     $(`#${eggNames[i]}`).stop().delay(generateRandom(1500, 0)).animate({
-  //       "margin-top":"+=496px"
-  //     }, generateRandom(3500, 2000), "swing");
-  //     console.log(`#${eggNames[i]}`);
-  //   }
+  $("button").on("click", function() {          // clicking on START button calls the function
 
-  $("button").on("click", function() {          // clicking on START button creates function
+    for (let i = 0; i <= numOfElements; i++) {    //loops through the eggs and assigns each a duration for animation
+     animateEggs(i);
+   };
 
-      $("#egg1").stop().delay(egg1Delay).animate({
-        "margin-top":"+=496px"
-      }, egg1Speed, "swing");
+    $(".cube").on("click", function(e) {      // when the egg is clicked, it dissapears
+      didntReachBottom += 1                   // the egg will not reach bottom and 1 is added to didntReacheBottom variable
+      $(this).css("opacity", "0");            // change opacity of the clicked item to hide it
 
-      $("#egg2").stop().delay(egg2Delay).animate({
-        "margin-top":"+=496px"
-      }, egg2Speed, "swing");
+        if (didntReachBottom == numOfElements) {         // check the number of eggs that did not reach the bottom, if it is equal to
+          alert("You win!");                             // alert player that they won
+        };
 
-      $("#egg3").stop().delay(egg3Delay).animate({
-        "margin-top":"+=496px"
-      }, egg3Speed, "swing");
-
-      $(".cube").on("click", function(e) {      // on click of the egg, it dissapears (will change image)
-        didntReacheBottom += 1                  // if it is clicked on, it will not reach bottom and 1 is added to didntReacheBottom variable
-        $(this).css("opacity", "0");            // change something about the item if it is clicked on (change color, later image)
-
-          if (didntReacheBottom > 2) {          // check if all boxes did not reach bottom
-            alert("You win!");                  // if true, alert player that they won
-          };
       });
-      //reset board
     });
+    //reset board
   });
